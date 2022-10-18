e19142188d09e2d1a2404da2d9e615788811d961
João Barbalho <joaobarbalho.contato@gmail.com>
2022-10-18 07:32:31 -0400

    first commit

---
+++ constants.py
@@ -0,0 +1,24 @@
+from pickle import PicklingError
+from game.shared.color import Color
+
+COLUMNS = 40
+ROWS = 20
+CELL_SIZE = 15
+MAX_X = 900
+MAX_Y = 600
+FRAME_RATE = 5
+FONT_SIZE = 15
+CAPTION = "cycle"
+CYCLE_LENGTH = 8
+WHITE = Color(255, 255, 255)
+RED = Color(255, 0, 0)
+YELLOW = Color(255, 255, 0)
+GREEN = Color(0, 255, 0)
+PINK = Color(255, 20, 147)
+CYAN = Color(0, 255, 255)
+
+# x component from where the score is displayed
+SCORE_X = 350
+
+# y component from where the score is displayed
+SCORE_Y = 0
---
+++ game/casting/score.py
@@ -0,0 +1,65 @@
+import constants
+from game.casting.actor import Actor
+from game.shared.point import Point
+
+class Score(Actor):
+    """
+    A record of points made or lost.
+    The responsibility of Score is to keep track of the points the player has earned.
+    It contains methods for adding and getting points.
+    Client should use get_text() to get string representation of the points earned.
+
+    Attributes:
+        _points (int)           : The points earned in the game.
+        position (Point)        : A point value to place each players score on the screen. 
+        _player_name (string)   : A string that will hold each players name.
+        set_position (method)   : A method that sets the scores position on the screen.
+        set_text (method)       : A method that displays each players name.
+    """
+    def __init__(self):
+        """
+        Sets starting points and position of points display
+        """
+        super().__init__()
+        self._points = 0
+        position = Point(0, 0)
+        self._player_name = ""
+        self.set_text(f"{self._player_name}: {self._points}")
+        self.set_position(position)
+
+    def add_points(self, points):
+        """
+        Adds the given points to the running total and updates the next.
+
+        Args:
+            self(Score)     : An instance of Score.
+            points (integer): The points to add.
+        """
+        self._points += points
+        self.set_text(f"{self._player_name}: {self._points}")
+
+    def get_points(self):
+        """
+        Sets the points for each user.
+
+        Returns:
+            Integer: A point value for each player.
+        """
+        return self._points
+    
+    def reduce_points(self):
+        """
+        Reduce points for the user.
+        """
+        self._points -= 1
+        self.set_text(f"{self._player_name}: {self._points}")
+
+    def set_player_name(self, name):
+        """
+        sets the player's name
+
+        Args:
+            name (string): gets the user inputted name
+        """
+        self._player_name = name
+        self.set_text(f"{self._player_name}: {self._points}")
